9847e89a505d99fa864e290149ac02d59d9ab06d
mohuneko <b0wdmohu@gmail.com>
2020-08-27 04:06:03 +0900

    add

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,9 @@
 Rails.application.routes.draw do
+  get 'inquiries/new'
+  get 'inquiries/confirm'
+  get 'inquiries/thanks'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'inquiries#new'
+  post 'inquiries/confirm'
+  post 'inquiries/thanks'
 end
